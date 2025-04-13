52deae3d63ece02e2b5ae3aeacccdc1c0dc7e5e6
UrbanoTrejoOrlando <orlandourbanotrejo@gmail.com>
2025-04-12 21:08:46 -0600

    Archivo principal

---
+++ api-2-1/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const PORT = 4000;
+
+// Inicializamos el servidor
+const app = express();
+
+// Cargamos los middlewares
+app.use(express.json());
+app.use(cors());
+
+app.get("/",(req,res)=>{
+    res.json({message:"Servidor 2-1"})
+})
+
+// Arrancamos el servicio
+app.listen(PORT,()=>{
+    console.log("SERVIDOR CORRIENDO EN EL PUERTO http://localhost:" + PORT);
+})
